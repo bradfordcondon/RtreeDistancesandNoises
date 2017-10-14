060e47d30e4c40d80d408e64ab3dcfdcaba21cc5
bradfordcondon <bradford.condon@gmail.com>
2017-10-13 22:23:31 -0400

    remove unnecessary functions

--- R/distance_trees.R
+++ R/distance_trees.R
@@ -1,4 +1,3 @@
-####### calculate the average distance between groups given a distance matrix and a clade list
 #' Calculate the average distance between groups given a distance matrix and a clade
 #' @param distanceMatrix The distance matrix
 #' @param cladeAssignments The clade assignments
@@ -36,14 +35,14 @@ withinGroupDistances <- function(distanceMatrix, cladeAssignments, ID_names) {
 }
 
 ########
-#' Bootstraps
-#' @param distanceMatrix hk
-#' @param numberOfTrees hkj
-#' @param withinSDtable hkj
-#' @param cladeAssignments hkj
-#' @param ID_names hkj
+#' Generates bootstraps trees based on within-group standard deviation.
+#' @param distanceMatrix A pairwise distance matrix.
+#' @param numberOfTrees The number of bootstrap trees to generate. Default: 1000
+#' @param withinSDtable The within clade SD table.
+#' @param cladeAssignments Ordered list of clade assignments for taxa.
+#' @param ID_names List of taxa names corresponding to the clade assignments.
 #'
-generateBootstrapTrees <- function(distanceMatrix, numberOfTrees, withinSDtable, cladeAssignments, ID_names) {
+generateBootstrapTrees <- function(distanceMatrix, numberOfTrees = 1000, withinSDtable, cladeAssignments, ID_names) {
 
     cladeList = data.frame(ID_names, cladeAssignments)
     bstrees = vector("list", 0)  #create blank object to put noised trees in
@@ -77,96 +76,3 @@ generateBootstrapTrees <- function(distanceMatrix, numberOfTrees, withinSDtable,
     }
     return(bstrees)
 }
-
-
-
-#takes a distance matrix, SD, and number of trees, and return the bootstrap trees for this SD via normal noising
-########
-#' noise
-#' @param originalMatrix hkjh
-#' @param standardDev kjhjkh
-#' @param numTrees hkjh
-#' @param rootTree kjhkj
-#'
-bootStrapWithNoiseFromNorm <- function(originalMatrix, standardDev, numTrees = 1000, rootTree = NULL) {
-    bstrees = vector("list", 0)  #create blank object to put noised trees
-    class(bstrees) <- "multiPhylo"
-    loop = c(1:numTrees)  #number of trees and intervals to make
-    for (i in loop) {
-        x <- originalMatrix  #reset matrix
-        rowsneeded = row.names(originalMatrix)  #reset taxa list
-        for (j in rowsneeded) {
-            rowsneeded = rowsneeded[rowsneeded != j]  #remove self from list
-            for (k in rowsneeded) {
-                oldval = x[j, k]
-                newval = oldval + rnorm(1, mean = 0, sd = standardDev)
-                x[j, k] <- newval
-                x[k, j] <- newval
-            }
-        }
-        x[x < 0] < -0  #set negative to zero
-        treex <- nj(as.dist(x))
-        treex$edge.length[treex$edge.length < 0] <- abs(treex$edge.length[treex$edge.length < 0])
-        if (!is.null(rootTree)) {
-            treex = ape::multi2di(treex)
-        }
-        bstrees[[i]] <- treex
-        message = paste("finished with tree number", i)
-        print(message)
-    }
-    return(bstrees)
-}
-
-#Goal: To take a range of SDs and a distance matrix as an input, noisea given number of rep trees, and to return the bootstrap values for each node in
-## list form as an output
-########
-#' Bootstraps
-#' @param originalMatrix hkjh
-#' @param rangeMin hkj
-#' @param rangeMax khj
-#' @param numTrees khj
-#' @param rootTree hkj
-#'
-bootStrapWithNoiseOverRange <- function(originalMatrix, rangeMin = 1, rangeMax = 1000, numTrees = 1000, rootTree = NULL) {
-    sdRange = c(rangeMin:rangeMax)  #range of SD to check
-    loop = c(1:numTrees)  #number of trees and intervals to make
-    bootstrapList = vector("list", 0)  #blank list of bootstrap values
-    mainTree = ape::nj(as.dist(originalMatrix))
-    mainTree$edge.length[mainTree$edge.length < 0] <- abs(mainTree$edge.length[mainTree$edge.length < 0])
-    for (a in sdRange) {
-        bstrees = vector("list", 0)  #create blank object to put noised trees
-        class(bstrees) <- "multiPhylo"
-        for (i in loop) {
-            x <- originalMatrix  #reset matrix
-            for (j in rowsneeded) {
-                others = rowsneeded[rowsneeded != j]  #remove self from list
-                for (k in others) {
-                  oldval = x[j, k]
-                  newval = oldval + rnorm(1, mean = 0, sd = a)
-                  x[j, k] <- newval
-                  x[k, j] <- newval
-                }
-            }
-            x[x < 0] < -0  #set negative to zero
-            treex <- nj(as.dist(x))
-            treex$edge.length[treex$edge.length < 0] <- abs(treex$edge.length[treex$edge.length < 0])
-            if (!is.null(rootTree)) {
-                treex = ape::root(treex, rootTree)
-            }
-            bstrees[[i]] <- treex
-        }
-        # perform bootstrap analysis
-        if (!is.null(rootTree)) {
-            clade_support <- prop.clades(mainTree, bstrees, rooted = TRUE)  #generate BS values based on trees
-        } else {
-            clade_support <- prop.clades(mainTree, bstrees, rooted = FALSE)  #generate BS values based on trees
-        }
-        clade_support = clade_support/numTrees * 100  #convert to percent
-        clade_support[is.na(clade_support)] <- 0  #set NA to zero
-        bootstrapList[[a]] <- clade_support
-        message = paste("finished with BS trees from SD ", a)
-        print(message)
-    }
-    return(bootstrapList)
-}
-
