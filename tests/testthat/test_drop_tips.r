2891cecdaee2d73e974db94c70fcfe4f9b6d1c28
bradfordcondon <bradford.condon@gmail.com>
2017-10-15 20:24:16 -0400

    full test

--- tests/testthat/test_drop_tips.r
+++ tests/testthat/test_drop_tips.r
@@ -1,11 +1,14 @@
-source("R/auxillary_functions.R")
 library("ape")
-
-test_that("Tips drop", {
+test_that("Tips dropped from reference tree", {
 start_length <- 10
   x <- rmtree(2, start_length)
   x[[2]]$tip.label[1] <- c("mismatch")  #tree 1 and tree 2 no longer match
   trimmed <- trimTreeToTree(x[[1]], x[[2]])
+  ## Did we return a tree?
   expect_equal( class(trimmed), "phylo")
-  expect_equal( length(trimmed), (start_length - 1) )
+  # Did the tree get smaller by 1?
+  expect_equal( length(trimmed$tip.label), (start_length - 1) )
+  ###trimTreeFromList
+  expect_equal(length(trimTreeFromList(x[[2]], c("mismatch"))$tip.label), 1)
+
 })
